4cd2687166408b9473aad84cd6ab082f87a0a920
Alexander Smorkalov <alexander.smorkalov@xperience.ai>
2024-05-30 16:15:39 +0300

    Update donation link.

--- setup.py
+++ setup.py
@@ -310,7 +310,7 @@ def main():
         cmake_source_dir=cmake_source_dir,
     )
 
-    print("OpenCV is raising funds to keep the library free for everyone, and we need the support of the entire community to do it. Donate to OpenCV on IndieGoGo:\nhttps://www.indiegogo.com/projects/opencv-5-support-non-profit-open-source-cv-ai#/")
+    print("OpenCV is raising funds to keep the library free for everyone, and we need the support of the entire community to do it. Donate to OpenCV on GitHub:\nhttps://github.com/sponsors/opencv\n")
 
 class RearrangeCMakeOutput:
     """

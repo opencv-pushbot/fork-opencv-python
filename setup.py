da63b3d64bf2dfdde64d3df6a8bc70217cf8eedf
Alexander Smorkalov <alexander.smorkalov@xperience.ai>
2024-06-02 19:17:05 +0300

    Syntax fix.

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ def main():
         +
         (
             [r"lib/libOrbbecSDK.dylib", r"lib/libOrbbecSDK.\d.\d.dylib", r"lib/libOrbbecSDK.\d.\d.\d.dylib"]
-            if sys.platform == "darwin" and
+            if sys.platform == "darwin"
             else []
         )
         +

12bf0c30c6244cbc511713b0d3967c5466c92f99
Alexander Smorkalov <alexander.smorkalov@xperience.ai>
2024-06-03 09:30:40 +0300

    Submodules update to tag 4.10.0.

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ def main():
         +
         (
             [r"lib/libOrbbecSDK.dylib", r"lib/libOrbbecSDK.\d.\d.dylib", r"lib/libOrbbecSDK.\d.\d.\d.dylib"]
-            if platform.system() == "darwin" and platform.machine() == "ARM64"
+            if platform.system() == "darwin" and platform.machine() == "arm64"
             else []
         )
         +
